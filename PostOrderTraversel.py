8783a359eaa888bee157c728e0387dd54fca704d
saurabhack <kamanesaurabh68@gmail.com>
2025-02-17 13:59:16 +0530

    Post order traversal of tree

---
+++ PostOrderTraversel.py
@@ -0,0 +1,27 @@
+class TreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.leftChild=None
+        self.rightChild=None
+
+root=TreeNode(1)
+even=TreeNode(2)
+odd=TreeNode(3)
+even1=TreeNode(4)
+even2=TreeNode(6)
+odd1=TreeNode(5)
+odd2=TreeNode(7)
+root.leftChild=even
+root.rightChild=odd
+even.leftChild=even1
+even.rightChild=even2
+odd.leftChild=odd1
+odd.rightChild=odd2
+
+def postOrderTraversal(node):
+    if not node:
+        return
+    postOrderTraversal(node.leftChild)
+    postOrderTraversal(node.rightChild)
+    print(node.data)
+postOrderTraversal(root)
